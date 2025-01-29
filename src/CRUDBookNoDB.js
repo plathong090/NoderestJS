3507292bd3f8efdca152bc31b9b232f4f5da0029
plathong090 <prangthong250947gmail.com>
2025-01-29 10:26:20 +0700

    show crud json

--- src/CRUDBookNoDB.js
+++ src/CRUDBookNoDB.js
@@ -68,4 +68,4 @@ app.delete('/books/:id',(req,res) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen(port , () => console.log(`http://localhost:${port}...`))
+app.listen(port , () => console.log(`Listening on port ${port}...`))
